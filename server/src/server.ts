3ebb8f30cd6f3c4811247532961ffadaedc0efb0
Gabriel Pupo <gabrielrspupo@gmail.com>
2022-06-08 01:34:56 -0300

    Dia 5 - configura CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://nlw-return-feedbacks-production.up.railway.app',
 }));
 app.use(express.json());
 
